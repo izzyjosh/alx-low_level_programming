4c1d120dbe0009acdd015b44c8f6329f2e129d88
Joshua <izzyjosh2@gmail.com>
2022-12-08 06:28:59 -0800

    new file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints \"programming is like building a multilingual puzzle
+ * @void: Function argument
+ * Return: 0 if the code runs successfully. 1 otherwise
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
