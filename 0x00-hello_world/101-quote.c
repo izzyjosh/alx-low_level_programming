94be01e4c4174f6889469fcb7fc723a20f3a015b
Joshua <izzyjosh2@gmail.com>
2022-12-08 20:12:17 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: parameter
+ * return: 1 (successful)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
