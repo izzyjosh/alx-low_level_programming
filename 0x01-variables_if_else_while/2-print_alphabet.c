1c2d08cab7ede79e02ccf0397be71126bf08ff19
Joshua <izzyjosh2@gmail.com>
2022-12-09 00:39:42 -0800

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: parameter
+ * Return: 0(successful)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar (10);
+
+	return (0);
+}
