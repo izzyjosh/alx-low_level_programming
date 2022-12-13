0421bd2405a608360cb897604a7a153864cba786
Joshua <izzyjosh2@gmail.com>
2022-12-13 03:56:15 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+	
+	return (0);
+}
