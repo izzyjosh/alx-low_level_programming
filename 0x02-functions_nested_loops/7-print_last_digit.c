0778a52a3dd89d39a343ea8c0c4e490818bc5899
Joshua <izzyjosh2@gmail.com>
2022-12-13 07:30:51 -0800

    last digut

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,10 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	int sum;
 
 	last_digit = abs(n % 10);
 
-	sum = printf("%d%d\n", last_digit, last_digit);
-	
-	return (sum);
+	printf("%d%d\n", last_digit, last_digit);
+
+	return (0);
 }
