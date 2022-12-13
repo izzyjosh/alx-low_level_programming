6f5fb2463a963fd320d05eff3d558383d9cc6924
Joshua <izzyjosh2@gmail.com>
2022-12-13 08:59:28 -0800

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,24 +10,24 @@
 void jack_bauer(void)
 {
 	int i, j, k, l;
-	
+
 	for (i = 0; i <= 2; i++)
 	{
-	      for (j = 0; j <= 9; j++)
-	      {
-	            if (i == 2 && j > 3)
-	            {
-	            }
-	            else
-	            {
-	                  for (k = 0; k < 6; k++)
-	                  {
-	                        for (l = 0; l <= 9; l++)
-	                        {
-	                              printf("%d%d:%d%d\n", i, j, k, l);
-	                        }
-	                  }
-	            }
-	      }
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == 2 && j >= 3)
+			{
+			}
+			else
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l <= 9; l++)
+					{
+						printf("%d%d:%d%d\n", i, j, k, l);
+					}
+				}
+			}
+		}
 	}
 }
