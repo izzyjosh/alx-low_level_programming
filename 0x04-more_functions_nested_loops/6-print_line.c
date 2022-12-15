409971d4fc8bc07bc073fac27d337d28432c7a71
Joshua <izzyjosh2@gmail.com>
2022-12-15 02:51:20 -0800

    correction

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,8 +10,15 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n == 0)
 	{
-		_putchar('_');
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
 }
