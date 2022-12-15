eb699d07b261d28743de4b73573e0ccb05da1ca0
Joshua <izzyjosh2@gmail.com>
2022-12-15 01:37:24 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ * @void: prameter
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
