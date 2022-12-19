b624232499f537566970030177195d8dc696d05c
Joshua <calebayuba222@gmail.com>
2022-12-19 05:05:31 -0800

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - put half of a string
+ * @str: parameter
+ */
+
+
+void puts_half(char *str)
+{
+	int i, len, start;
+
+	len = strlen(str);
+
+	if (len % 2 == 1)
+	{
+		start = (len - 1) / 2;
+		for (i = start; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		start = len / 2;
+		for (i = start; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
