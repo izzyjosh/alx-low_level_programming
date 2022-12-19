c01cf42e1b087cc5b1932d516d1bf62b752437dc
Joshua <calebayuba222@gmail.com>
2022-12-19 06:11:55 -0800

    correctuon

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,7 +16,7 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; i < len && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for ( ; i < len; i++)
-		dest[i] = '\0';
+	dest[i] = '\0';
+
 	return (dest);
 }
