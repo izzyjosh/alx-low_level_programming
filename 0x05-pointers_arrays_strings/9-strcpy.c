24292eaa361364c4dc55a60a1ecbb550acba5571
Joshua <calebayuba222@gmail.com>
2022-12-19 05:58:24 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy and paste
+ * @dest: paste here
+ * @src: copy from here
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = strlen(src);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
