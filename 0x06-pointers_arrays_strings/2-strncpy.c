ffd2d36a0983de1300b7409037e28f7f30a174a2
Joshua <calebayuba222@gmail.com>
2022-12-21 13:26:47 -0800

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * _strncpy - copy from src to dest
- * @dest: paste in dest
- * @src: copy from
- * Return: dest
+ * _strncpy - copies src into dest and characters must
+ * be equal to n.
+ * @dest: string to be copied into
+ * @src: string to be copied from
+ * @n: number of characters to be copied
+ * Return: pointer to dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; *(src + i) && i < n; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
+
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
