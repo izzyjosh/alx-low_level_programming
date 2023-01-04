7ed086fcf423b11721b3b2329ceba6d544594d02
Joshua <calebayuba222@gmail.com>
2023-01-04 08:41:23 -0800

    modification

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,3 +4,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_recursion(int n);
+int _check_for_sqrt(int n, int guess);
