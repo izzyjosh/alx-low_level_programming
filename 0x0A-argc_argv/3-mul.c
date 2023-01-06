b87530328d98bcb1772ee1f4c80f8b71ba1d0319
Joshua <calebayuba222@gmail.com>
2023-01-06 05:39:57 -0800

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (successful)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul, j, k;
+
+	if (argc > 1)
+	{
+		j = atoi(argv[1]);
+		k = atoi(argv[2]);
+		mul = j * k;
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
