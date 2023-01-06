70cb687b791808c73256f4d0f8df851b6f79f07b
Joshua <calebayuba222@gmail.com>
2023-01-06 05:22:51 -0800

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,21 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int sum, i;
+	(void)argv;
+	printf("%d\n", argc - 1);
 
-	sum = 0;
-
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum += 1;
-		}
-		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("%d\n", sum);
-	}
 	return (0);
 }
