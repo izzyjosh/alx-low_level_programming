ddfd313b057e21faacb97b0e2770fad1cfa543e2
Joshua <calebayuba222@gmail.com>
2023-01-06 04:43:48 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: count the number of argument at executing time
+ * @argv: an array containing each argument
+ * Return: int
+ */
+
+int main(int argc, char * argv[])
+{
+	int i;
+
+	i = 0;
+
+	_putchar(argv[i]);
+
+	return (0);
+}
