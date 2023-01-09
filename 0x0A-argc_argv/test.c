302b590954b37c38ed0badeef4345990a9a43df6
Joshua <calebayuba222@gmail.com>
2023-01-09 04:08:55 -0800

    add

---
+++ 0x0A-argc_argv/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	int sum, i;
+	
+	sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += 1;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", sum);
+	}
+	return (0);
+}
