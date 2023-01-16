0e2610a9644159124b8d08d1adb530fb2b75f204
Joshua <calebayuba222@gmail.com>
2023-01-16 03:52:42 -0800

    correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,13 @@
+#ifndef HEADER
+#define HEADER
+
 /**
- * struct dog - define the attribute of dog
+ * struct dog - a struct object
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Description: a dog object that holds data for name, age and
+ * owner of the dog.
  */
 
 struct dog
@@ -10,7 +15,12 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-
-}
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
