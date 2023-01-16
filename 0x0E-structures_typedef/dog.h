e0e59f753e2c35757e084aa880b91d7be0ba0fce
Joshua <calebayuba222@gmail.com>
2023-01-16 02:32:37 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - define the attribute of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
